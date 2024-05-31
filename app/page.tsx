df6791ed39199ab62d0a6111f2a7c5b907550192
timeach <timeach@gmail.com>
2024-05-31 11:33:28 +0800

    before create invoice

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-<div className={styles.shape} />;
-
-
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <div className={styles.shape} />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
